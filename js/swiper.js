9121d442e94c50705ec47217cfe93b5a8178a503
isaacprince <isaacprince002@gmail.com>
2024-02-15 03:17:50 -0500

    feat: added new pages

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+// javascript document 
+// swiper script 
+
+var swiper = new Swiper ('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
